42d78766703a3e708eb355adb21fa0c300d1dd3d
N Vinay Kumar Reddy <111642478+Nvinay-2003@users.noreply.github.com>
2024-12-21 12:01:12 +0000

    yeah

--- de_project_1/app/create_task.py
+++ de_project_1/app/create_task.py
@@ -33,6 +33,12 @@ with DAG("dag_copy_emp",schedule=timedelta(days=1),stage_location='@dev_deployme
     packages=["snowflake-snowpark-python"],imports=["@dev_deployment/my_de_project_1/app.zip"],\
     stage_location="@dev_deployment"),warehouse="compute_wh")
   
+  dag_task_2 =  DAGTask("execute_sql_statements",StoredProcedureCall(procedures.execute_sql_statements,\
+    packages=["snowflake-snowpark-python"],imports=["@dev_deployment/my_de_project_1/app.zip"],\
+    stage_location="@dev_deployment"),warehouse="compute_wh")
+  
+  dag_task_1>>dag_task_2
+  
   schema = root.databases["demo_db"].schemas["public"]
   dag_op = DAGOperation(schema)
   dag_op.deploy(dag,CreateMode.or_replace)
